63272a03d8a27fd126fe6e5d04a44940d51641db
Aman Verma <ehaan1026@gmail.com>
2026-02-06 14:38:11 +0530

    Add spring security dependency

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+/*
 @Configuration
 public class SecurityConfig {
 
@@ -13,3 +13,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+*/
